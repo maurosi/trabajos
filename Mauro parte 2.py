06c0ffae84ec25df95685d099414f4b807a2237a
maurosi <85770786+maurosi@users.noreply.github.com>
2021-06-12 01:08:07 -0300

    Add files via upload

---
+++ Mauro parte 2.py
@@ -0,0 +1,18 @@
+n=int(input("ingrese un numero:"))
+
+if(n==2):
+    print("es primo")
+elif(n==3):
+    print("es primo")
+elif(n==5):
+    print("es primo");
+else:
+    if(n%2==0):
+        print("no es primo")
+    elif(n%3==0):
+        print("no es primo")
+    elif(n%5==0):
+                print("no es primo")
+    else:
+        print("es primo")
+    
